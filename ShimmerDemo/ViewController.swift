1ac5a2a414b7e66df12eb94e64be5e71efcee4b9
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-01-25 18:22:11 +0530

    created new xcode project

---
+++ ShimmerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShimmerDemo
+//
+//  Created by Ashish Badak on 25/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
